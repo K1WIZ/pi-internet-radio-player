409edd99024003aef16f685d4f325d163d03831e
John Rogers <k1wiz@wizworks.net>
2023-08-10 12:28:15 -0400

    Update play.php
    added a stop playback button

--- play.php
+++ play.php
@@ -11,23 +11,33 @@ if (($handle = fopen($csvFilePath, 'r')) !== false) {
     fclose($handle);
 }
 
+// Function to kill stream if stop requested
+function stopStream() {
+    shell_exec('sudo /usr/bin/killall vlc');
+}
+
 // Function to play selected stream and kill existing mplayer processes
 function playStream($url) {
-    //shell_exec('sudo /usr/bin/killall mplayer');   // changed from mplayer to vlc to support https streams
-    shell_exec('sudo /usr/bin/killall vlc');
-    //$command = "sudo /usr/bin/screen -m -d mplayer -ao alsa:device=hw=3.0 $url";  // changed from mplayer to vlc to support https streams
+    stopStream();
     $command = "sudo runuser -l orangepi -c 'cvlc --alsa-audio-device default:CARD=USB $url >/dev/null 2>&1 &'";
     shell_exec($command);
 }
 
 // Handle form submission
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $selectedRecord = $_POST["recordNumber"];
-    if (isset($stations[$selectedRecord])) {
-        $selectedUrl = $stations[$selectedRecord];
-        playStream($selectedUrl);
-    } else {
-        $errorMessage = "Invalid record number.";
+    if (isset($_POST["tune"])) {
+        // "TUNE" button clicked
+        $selectedRecord = $_POST["recordNumber"];
+        if (isset($stations[$selectedRecord])) {
+            $selectedUrl = $stations[$selectedRecord];
+            playStream($selectedUrl);
+        } else {
+            $errorMessage = "Invalid record number.";
+        }
+    } elseif (isset($_POST["stopPlayback"])) {
+        // "STOP PLAYBACK" button clicked
+        stopStream();
+        $playbackStopped = true;
     }
 }
 ?>
@@ -35,28 +45,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Stream Player</title>
 </head>
 <body>
-    
     <?php if (isset($errorMessage)): ?>
         <p style="color: red;"><?php echo $errorMessage; ?></p>
     <?php endif; ?>
-    <!--   removed the table as it's already on the index
-    <h2>Saved Station Presets:</h2>
-    <table border="1">
-        <tr>
-            <th>Preset Number</th>
-            <th>Stream URL</th>
-        </tr> 
-        <?php foreach ($stations as $recordNumber => $url): ?>
-            <tr>
-                <td><?php echo $recordNumber; ?></td>
-                <td><?php echo $url; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-    -->
+    <?php if (isset($playbackStopped)): ?>
+        <p style="color: green;">Playback stopped.</p>
+    <?php endif; ?>
     <form method="post" action="">
         <label for="recordNumber">Select a station preset to play:</label>
         <select name="recordNumber" id="recordNumber">
@@ -64,8 +60,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <option value="<?php echo $recordNumber; ?>"><?php echo $recordNumber; ?></option>
             <?php endforeach; ?>
         </select>
-        <button type="submit">TUNE</button>
+        <button type="submit" name="tune">TUNE</button>
+        <button type="submit" name="stopPlayback">STOP PLAYBACK</button>
     </form>
 </body>
 </html>
-
